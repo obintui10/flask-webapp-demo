b75a3c7b97f14e60a78f92cd0edacb896f38c988
Obi Ayuk Ntui <ayukspecial@gmail.com>
2026-05-10 02:09:09 +0530

    Create app.js

---
+++ static/app.js
@@ -0,0 +1,9 @@
+// static/app.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector(".container");
+
+    container.addEventListener("click", function() {
+        alert("Hello from Flask WebApp Demo!");
+    });
+});
